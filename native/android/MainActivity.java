680ce5f16435497b1f8df9236a0bd6d394f7a71f
mauriziocreate <65018346+mauriziocreate@users.noreply.github.com>
2026-07-30 17:23:37 +0200

    Add files via upload

--- native/android/MainActivity.java
+++ native/android/MainActivity.java
@@ -1,6 +1,11 @@
 package info.dandreart.mairgo;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
@@ -8,5 +13,17 @@ public class MainActivity extends BridgeActivity {
     public void onCreate(Bundle savedInstanceState) {
         registerPlugin(MairBackupPlugin.class);
         super.onCreate(savedInstanceState);
+
+        // Barra di stato ben distinta: fondo scuro e icone bianche.
+        // Il contenuto web continua a rispettare le safe-area già presenti nel CSS.
+        Window window = getWindow();
+        window.setStatusBarColor(Color.rgb(43, 33, 29));
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            View decorView = window.getDecorView();
+            int flags = decorView.getSystemUiVisibility();
+            flags &= ~View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
+            decorView.setSystemUiVisibility(flags);
+        }
     }
 }
